eee1b5bd58270206984b4fb0ffe5738a8c4f5260
Drarkors <rafaelfzanetti@hotmail.com>
2024-06-11 14:31:09 -0300

    :tada: Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// DayJS config
+import "./libs/dayjs.js";
+
+// CSS
+import "./styles/global.css";
+
+// JS
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  target: "web",
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html"),
+        favicon: path.resolve(__dirname, "src", "assets", "logo.svg"),
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "src", "assets"),
+            to: path.resolve(__dirname, "dist", "src", "assets"),
+          },
+        ],
+      }),
+    ],
+  },
+};
