6e8b82b496fe58be296867bee6b109d45303a37c
Drarkors <rafaelfzanetti@hotmail.com>
2024-06-11 19:38:32 -0300

    :lipstick: Adding header and client identification

--- src/main.js
+++ src/main.js
@@ -5,5 +5,11 @@ import "./libs/dayjs.js";
 
 // CSS
 import "./styles/global.css";
+import "./styles/input.css";
+import "./styles/button.css";
+import "./styles/header.css";
+import "./styles/main.css";
+import "./styles/client-card.css";
+import "./styles/member-card.css";
 
 // JS
